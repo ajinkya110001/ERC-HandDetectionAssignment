27f8ab6643225c3fd9e07d54d37cbb9ec5171b08
ChocoCosmos <148470837+ChocoCosmos@users.noreply.github.com>
2023-11-05 05:38:12 +0530

    implemented hand detection
    implemented and labelled hand detection using the mediapipe framework

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import cv2
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 cap=cv2.VideoCapture(0)
-#TODO: Add required mediapipe functions
+base_options = python.BaseOptions(model_asset_path=(r'model\hand_landmarker.task'))
+options = vision.HandLandmarkerOptions(base_options=base_options,num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+def hand_detection_and_label(image,detection):
+    landmarks_list = detection.hand_landmarks
+    image_height,image_width, image_channels = image.shape 
+    for i,j in enumerate(landmarks_list):
+        x = min([coord.x for coord in j])*image_width
+        y = min([coord.y for coord in j])*image_height
+        cv2.putText(image,f"{(detection.handedness[i][0].category_name)}",(int(x),int(y)),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,255,255),thickness=2, lineType=cv2.LINE_AA)
+
 
 while True:
     success, frame=cap.read()
+
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    detection_result = detector.detect(image)
+    hand_detection_and_label(frame,detection_result)
     
     cv2.imshow("Hand Detection",frame)
-
-    #TODO: Implement the functions
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
