7e561779a66a2ff59b38dbd90073fcaeba6c1a0d
tarunpratapsingh17 <148236519+tarunpratapsingh17@users.noreply.github.com>
2023-11-09 01:48:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ import cv2
 import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+mp_hands = mp.solutions.hands #it is an instance of mediapipe's hand module 
+hands = mp_hands.Hands() #it detects/analyses hands 
 
 
 while True:
     success, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    
-    results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    frame = cv2.flip(frame, 1) #it flips the input video/image capture 
+    rgbframe=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)#here we are changing the bgr format to rgb as required by mediapiper
+    results = hands.process(rgbframe) # this process the rgbframe
 
     if results.multi_handedness:
-        for idx, handedness in enumerate(results.multi_handedness):
-            hand_label = handedness.classification[0].label 
-            hand_score = handedness.classification[0].score 
-            if hand_score > 0.5:  
+        for handedness in (results.multi_handedness):
+            hand_label = handedness.classification[0].label #used to detect orientation left or right 
                 
-                cv2.putText(frame, hand_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+      cv2.putText(frame, hand_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) #puts the text on the output screen
    
     cv2.imshow('Hand Detection', frame)
 
