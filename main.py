a18e9f44de3e8c11bdb82bd23e99eb33bfc86d54
Harsh <harshbhatia1605@gmail.com>
2023-11-09 13:41:27 +0530

    Add color to hand landmarks and refactor some code

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import cv2
 import mediapipe as mp
 
-# Initialize MediaPipe Hands.
+# Constants
+WINDOW_NAME = "Hand Tracking"
+
+# Initialize MediaPipe Hands and Drawing utility.
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands()
 
-# Start capturing video from the webcam.
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = video_capture.read()
-
+def process_frame(frame):
     # Flip the frame and convert it from BGR to RGB.
     frame = cv2.flip(frame, 1)
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -19,14 +18,23 @@ while True:
     # Process the frame with MediaPipe Hands.
     result = hands.process(frameRGB)
 
-    # If hand landmarks are detected, classify each hand as left or right.
+    # If hand landmarks are detected, classify each hand as left or right and draw landmarks.
     if result.multi_hand_landmarks:
         for idx, hand_landmarks in enumerate(result.multi_hand_landmarks):
             # Determine if this hand is left or right.
             handedness = result.multi_handedness[idx].classification[0].label
 
-            # Draw the hand landmarks and connections.
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            # Choose color based on handedness.
+            color = (0, 255, 0) if handedness == "Right" else (0, 0, 255)
+
+            # Draw the hand landmarks and connections with the chosen color.
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(color=color),
+                mp_drawing.DrawingSpec(color=color),
+            )
 
             # Display the label at the wrist landmark.
             cv2.putText(
@@ -42,19 +50,34 @@ while True:
                     ]
                 ),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                2,
+                1,
                 (255, 255, 255),
-                3,
+                2,
                 cv2.LINE_AA,
             )
 
-    # Display the frame with the detected hand landmarks.
-    cv2.imshow("Hand Tracking", frame)
+    return frame
 
-    # If the 'q' key is pressed, break the loop and stop the video capture.
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
 
-# Release the video capture and close all windows.
-video_capture.release()
-cv2.destroyAllWindows()
+def main():
+    # Start capturing video from the webcam.
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = video_capture.read()
+        frame = process_frame(frame)
+
+        # Display the frame with the detected hand landmarks.
+        cv2.imshow(WINDOW_NAME, frame)
+
+        # If the 'q' key is pressed, break the loop and stop the video capture.
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    # Release the video capture and close all windows.
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
