ef89f5ef4433b227c9a87e9998e7f9553b9e0301
Harsh <harshbhatia1605@gmail.com>
2023-11-09 14:13:22 +0530

    Add window centering functionality using tkinter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 import cv2
 import mediapipe as mp
 
@@ -63,12 +65,29 @@ def main():
     # Start capturing video from the webcam.
     video_capture = cv2.VideoCapture(0)
 
+    # Get the screen size
+    root = tk.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    root.destroy()
+
     while True:
         ret, frame = video_capture.read()
         frame = process_frame(frame)
 
         # Display the frame with the detected hand landmarks.
         cv2.imshow(WINDOW_NAME, frame)
+
+        # Get the window size
+        window_width = frame.shape[1]
+        window_height = frame.shape[0]
+
+        # Calculate the position to place the window in the center of the screen
+        position_x = (screen_width // 2) - (window_width // 2)
+        position_y = (screen_height // 2) - (window_height // 2)
+
+        # Move the window to the center of the screen
+        cv2.moveWindow(WINDOW_NAME, int(screen_width / 6), int(screen_height / 6.5))
 
         # If the 'q' key is pressed, break the loop and stop the video capture.
         if cv2.waitKey(1) & 0xFF == ord("q"):
