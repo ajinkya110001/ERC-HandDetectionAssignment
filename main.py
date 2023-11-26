a7aeab0d526043fe798ac7df3f84d0da9c6fe41f
unknown <f20231031@goa.bits-pilani.ac.in>
2023-11-26 22:28:41 +0530

    modification

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ cap=cv2.VideoCapture(0)
 
 while True:
     success, frame=cap.read()
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+    if results.multi_hand_landmarks:
+        for landmarks in results.multi_hand_landmarks:
+            for point in landmarks.landmark:
+                height, width, _ = frame.shape
+                cx, cy = int(point.x * width), int(point.y * height)
+                cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
     
     cv2.imshow("Hand Detection",frame)
 
